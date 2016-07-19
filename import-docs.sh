428e0e07c94597c66804bed5d6aae3f794b4ade1
Robin Winslow <robin@robinwinslow.co.uk>
2016-07-19 09:47:22 +0200

    Show docs revision

--- import-docs.sh
+++ import-docs.sh
@@ -4,6 +4,11 @@ set -e
 
 rm -rf docs
 git clone --depth=1 --branch=new-structure git@github.com:CanonicalLtd/snappy-docs.git docs
-rm -rf docs/.git
+(
+  cd docs
+  echo "Checked out docs at commit:"
+  git rev-parse HEAD
+  rm -rf .git
+)
 mv docs/navigation.html _includes/docs-navigation.html
 ./update-docs-layout.py
